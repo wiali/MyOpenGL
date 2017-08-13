b30c3329a5db4023943f289ce2e7a5d245fca6c8
Aden <Aden@163.com>
2017-08-14 02:15:47 +0800

    clean code

--- ink_layer_glwidget.h
+++ ink_layer_glwidget.h
@@ -156,10 +156,6 @@ private:
 
     QVector3D normalize(QVector3D& vec3);
 
-    void initcor(int &dx, int &dy, int &x, int& y, int &p, int &xs, int &ys, int& xe, int& ye);
-    void myLine(int xs, int ys, int xe, int ye);
-    void display();
-
 private:
     GLuint m_posAttr;
     GLuint m_colAttr;
